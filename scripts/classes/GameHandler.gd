86cfff87d628748491face31cab21ad243221466
huhthatsnice <65060552+huhthatsnice@users.noreply.github.com>
2026-01-15 22:56:58 -0700

    remove pausing in lobbies

--- scripts/classes/GameHandler.gd
+++ scripts/classes/GameHandler.gd
@@ -342,7 +342,7 @@ func _process(_dt: float) -> void:
 #endregion
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"pause"):
+	if event.is_action_pressed(&"pause") and SSCS.lobby == null:
 		if playing:
 			pause()
 		else:
